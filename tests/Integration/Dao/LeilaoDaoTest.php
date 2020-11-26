d0fab612aa73d5c50faed00dbf6320dccaf7741b
Cezar Rosa <cecez86@gmail.com>
2020-11-26 09:53:44 -0300

    * corrigindo erro em SQL * remocao de tuplas no banco de dados ao finalizar o teste (tear down)

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -13,17 +13,24 @@ class LeilaoDaoTest extends TestCase
 {
 
     public function testInsercaoEBuscaDevemFuncionar() {
-        $leilao = new Leilao('Apple Pencil 2020');
-        $leilaoDao = new LeilaoDao(ConnectionCreator::getConnection());
+        // arrange
+        $leilao     = new Leilao('Apple Pencil 2020');
+        $pdo        = ConnectionCreator::getConnection();
+        $leilaoDao  = new LeilaoDao($pdo);
 
+        // act
         $leilaoDao->salva($leilao);
         $leiloes = $leilaoDao->recuperarNaoFinalizados();
 
+        // assert
         self::assertCount(1, $leiloes);
         self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
 
         // assertSame: verifica conteúdo e tipo (===)
         self::assertSame('Apple Pencil 2020', $leiloes[0]->recuperarDescricao());
+
+        // tear down
+        $pdo->exec('DELETE FROM leiloes');
     }
 
 }
